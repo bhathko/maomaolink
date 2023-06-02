97798b4673a1dadc4368dbbe7a8e39cf90ba4e4f
bhathko <bhathko@gmail.com>
2023-06-03 00:58:25 +0800

    feat(link page): build the link page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AvatarComponent } from './components/avatar/avatar.component';
+import { LinkListComponent } from './components/link-list/link-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AvatarComponent,
+    LinkListComponent
   ],
   imports: [
     BrowserModule,
